4dea28e58994a54d102c358d96ba9fedf8def6d0
Gitanjali Chittar <gitanjalichittar.oms@gmail.com>
2024-02-21 19:09:24 +0530

    change api

--- src/main/java/Demo/ThirdParty/Controller/ResultController.java
+++ src/main/java/Demo/ThirdParty/Controller/ResultController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api1")
 public class ResultController {
     @Autowired
     private  ResultService resultService;
